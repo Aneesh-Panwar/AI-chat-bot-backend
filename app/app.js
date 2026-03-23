cf6ed5caf492d66cf3a496b9000c5eecf2bd73a8
Aneesh-Panwar <aaneeshpanwar@gmail.com>
2026-03-23 12:45:25 +0530

    backend test routes setup and testing

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import chatRoutes from "./routes/chatRoutes.js";
+import testRoutes from "./routes/testRoutes.js";
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", chatRoutes);
+app.use("/api", testRoutes);
 
 export default app;
